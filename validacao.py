9ea872b716b72019c82f2b3c1513c8de7fcaeb58
Guilherme <gui_souza_123@hotmail.com>
2021-05-07 19:33:53 -0300

    Usuários finalizados

---
+++ validacao.py
@@ -0,0 +1,29 @@
+def valida_diretor(nome_completo):
+    if len(nome_completo) == 0:
+        return False
+
+    return True
+
+def valida_genero(nome):
+    if len(nome) == 0:
+        return False
+
+    return True
+
+def valida_filmes(titulo, ano, classificacao, preco, ):
+    if len(nome_completo) == 0:
+        return False
+
+    if len(CPF) != 14:
+        return False
+
+    return True
+
+def valida_usuario(nome_completo, CPF):
+    if len(nome_completo) == 0:
+        return False
+
+    if len(CPF) != 14:
+        return False
+
+    return True
